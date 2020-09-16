cb9f852cfc503372892faae28e96dba5813250bf
mamirad <amirad2020@gmail.com>
2020-09-16 03:59:40 +0000

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	  accepts_nested_attributes_for :phone_numbers, :allow_destroy => true
 	has_many :user_phone_numbers
   has_many :phone_numbers, through: :user_phone_numbers
 end
